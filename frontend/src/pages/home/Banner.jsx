cc8d6d4859b10c04b61d7982f6c85786aeef81ae
BuliDev <contact@alenburovic.com>
2024-12-27 22:35:06 +0100

    Add Banner component

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router'
+import bannerImg from '../../assets/header.png'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='section-container header-container'>
+        <div className='header-content z-30'>
+            <h4 className='uppercase'>UP TO 20% Discount on</h4>
+            <h1>Bike Helmets</h1>
+            <p>Discover our wide range of high-quality motorcycle helmets designed for safety and comfort. Shop now and save up to 20% on select models.</p>
+            <button className='btn'>
+                <Link to='/shop'>
+                    EXPLORE NOW
+                </Link>
+            </button>
+        </div>
+        <div className='header-image'>
+            <img src={bannerImg} alt="banner image" />
+        </div>
+    </div>
   )
 }
 
