7b2016fd54e81e0e9cb03e7fbf14174c238b4473
BuliDev <contact@alenburovic.com>
2025-01-05 19:59:58 +0100

    Make Add to Cart working on SingleProduct component

--- frontend/src/pages/shop/productDetails/SingleProduct.jsx
+++ frontend/src/pages/shop/productDetails/SingleProduct.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import RatingStar from '../../../components/RatingStar'
+import products from '../../../data/products.json'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../../../redux/features/cart/cartSlice'
 
 const SingleProduct = () => {
   const { id } = useParams()
+
+  const product = products.find((product) => product.id === parseInt(id))
+
+  if (!product) {
+    return <p>Product not found</p>
+  }
+
+  const dispatch = useDispatch()
+  const handleAddToCart = () => {
+    dispatch(addToCart(product))
+  }
 
   return (
     <>
@@ -18,7 +32,7 @@ const SingleProduct = () => {
             <Link to='/shop'>shop</Link>
           </span>
           <i className='ri-arrow-right-s-line'></i>
-          <span className='hover:text-primary'>product name</span>
+          <span className='hover:text-primary'>{product.name}</span>
         </div>
       </section>
 
@@ -26,32 +40,35 @@ const SingleProduct = () => {
         <div className='flex flex-col items-center md:flex-row gap-8'>
           <div className='md:w-1/2 w-full'>
             <img
-              src='https://images.unsplash.com/photo-1512201078372-9c6b2a0d528a?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              src={product.image}
               alt=''
               className='rounded-md w-full h-auto'
             />
           </div>
           <div className='md:w-1/2 w-full'>
-            <h3 className='text-2xl font-semibold mb-4'>Product Name</h3>
+            <h3 className='text-2xl font-semibold mb-4'>{product.name}</h3>
             <p className='text-xl text-primary mb-4'>
-              $100 <s>$130</s>
+              ${product.price} <s>${product.oldPrice}</s>
             </p>
-            <p className='text-gray-400 mb-4'>This is an product description</p>
+            <p className='text-gray-400 mb-4'>{product.description}</p>
 
             <div>
               <p>
-                <strong>Category:</strong> accessories
+                <strong>Category:</strong> {product.category}
               </p>
               <p>
-                <strong>Color:</strong> beige
+                <strong>Color:</strong> {product.color}
               </p>
               <div className='flex gap-1 items-center'>
                 <strong>Rating:</strong>
-                <RatingStar rating={'4'} />
+                <RatingStar rating={product.rating} />
               </div>
             </div>
 
-            <button className='mt-6 px-6 py-3 bg-primary text-white rounded-md'>
+            <button
+              onClick={handleAddToCart}
+              className='mt-6 px-6 py-3 bg-primary text-white rounded-md'
+            >
               Add to Cart
             </button>
           </div>
